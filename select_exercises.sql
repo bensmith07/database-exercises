4829da1cabb7f4dd7b6dda6699c3d103d28b27a4
Ben Smith <ben.f.smith07@gmail.com>
2022-01-07 10:35:09 -0600

    edit select_exercises.sql

--- select_exercises.sql
+++ select_exercises.sql
@@ -35,7 +35,7 @@ SELECT name
 -- 4b. The year Sgt. Pepper's Lonely Hearts Club Band was released
 SELECT release_date
   FROM albums
-  WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
+  WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 
 -- 4c. The genre for the album Nevermind
 SELECT genre 
@@ -58,6 +58,7 @@ SELECT name
 -- ANSWER: this query returns results for albums whose genre
 --         is listed as the specific string 'Rock'. 
 --         'Hard Rock' and 'Progressive Rock' are different strings.
+
 SELECT name
   FROM albums
   WHERE genre = 'Rock';
