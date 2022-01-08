82d8b7cef56046cac14d5db310c6bd27c667ae1a
Ben Smith <ben.f.smith07@gmail.com>
2022-01-08 12:57:30 -0600

    join_exercises.sql

--- join_exercises.sql
+++ join_exercises.sql
@@ -42,11 +42,49 @@ USE employees;
 -- 2. Write a query that shows each department along with
 --    the name of the current manager for that department. 
 
-SELECT d.dept_name AS department,
-       CONCAT(e.first_name, ' ', e.last_name) as manager
+SELECT d.dept_name AS 'Department Name',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
   FROM employees AS e
     JOIN dept_manager AS dm 
-      ON dm.emp_no = e.emp_no
+      ON e.emp_no = dm.emp_no
     JOIN departments AS d 
+      ON dm.dept_no = d.dept_no
+  WHERE dm.to_date = '9999-01-01'
+  ORDER BY d.dept_name;
+
+-- 3. Find the name of all departments currently
+--    managed by women. 
+
+SELECT d.dept_name AS 'Department',
+       CONCAT(e.first_name, ' ', e.last_name) AS 'Manager'
+  FROM departments AS d 
+    JOIN dept_manager as dm
       ON d.dept_no = dm.dept_no
-  WHERE dm.to_date = '9999-01-01';
+    JOIN employees as e
+      ON dm.emp_no = e.emp_no 
+  WHERE e.gender = 'F'
+    AND dm.to_date = '9999-01-01'
+  ORDER BY d.dept_name;
+
+-- 4. Find the current title of employees currently
+--    working in the Customer Service Department
+
+SELECT t.title, COUNT(t.emp_no)
+  FROM titles AS t 
+    JOIN employees AS e 
+      ON t.emp_no = e.emp_no
+    JOIN dept_emp AS de
+      ON e.emp_no = de.emp_no
+    JOIN departments AS d 
+      ON de.dept_no = d.dept_no 
+  WHERE t.to_date = '9999-01-01'
+    AND de.to_date = '9999-01-01'
+    AND de.dept_no = 'd009'
+  GROUP BY t.title
+  ORDER BY t.title;
+
+-- 5. Find the current salary of all current managers. 
+
+SELECT 
+
+
