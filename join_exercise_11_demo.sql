d4ef146b829f721e62fd6897cb0f3dd47742c6bc
Ben Smith <ben.f.smith07@gmail.com>
2022-01-12 11:41:42 -0600

    join_exercise_11_demo.sql

--- join_exercise_11_demo.sql
+++ join_exercise_11_demo.sql
@@ -1,4 +1,3 @@
-
 -- STEP 1: Find the names of all employees...
 
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee'
@@ -6,11 +5,20 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee'
 
   FROM employees AS e 
 
+
+
+
+
+
+
+
+
 ----------------------------------------------------------
 -- STEP 2: Find the names of all employees...
 --         WHO ARE CURRENT EMPLOYEES
 
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee'
+
 
 
   FROM employees AS e
@@ -45,7 +53,6 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee',
   SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee',
        d.dept_name
 
-
   FROM employees AS e
     JOIN dept_emp AS de 
       ON e.emp_no = de.emp_no
@@ -53,19 +60,15 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee',
       ON de.dept_no = d.dept_no 
     JOIN dept_manager AS dm 
       ON d.dept_no = dm.dept_no 
-     
-
 
   WHERE de.to_date = '9999-01-01' 
     AND dm.to_date = '9999-01-01' 
 
 ----------------------------------------------------------
 -- STEP 5: Find that manager's name 
-
 SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee',
        d.dept_name AS 'Department',
        CONCAT(mgr.first_name, ' ', mgr.last_name) AS 'Mgr Name'
-
 
   FROM employees AS e
     JOIN dept_emp AS de 
@@ -76,8 +79,6 @@ SELECT CONCAT(e.first_name, ' ', e.last_name) AS 'Employee',
       ON d.dept_no = dm.dept_no
     JOIN employees AS mgr
       ON dm.emp_no = mgr.emp_no
-     
-
 
   WHERE de.to_date = '9999-01-01' 
     AND dm.to_date = '9999-01-01' ;
