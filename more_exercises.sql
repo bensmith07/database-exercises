062cfcf8134b3ff0ea7fc12ab7d19472641b845f
Ben Smith <ben.f.smith07@gmail.com>
2022-01-12 10:19:45 -0600

    more_exercises.sql

--- more_exercises.sql
+++ more_exercises.sql
@@ -368,3 +368,150 @@ SELECT c.name AS 'Genre',
   GROUP BY c.name
   ORDER BY SUM(p.amount) DESC
   LIMIT 5;
+
+
+
+-- 1. SELECT statements
+
+-- 1a. Select all columns from the actor table.
+SELECT * 
+  FROM actor;
+-- 1b. Select only the last_name column from the actor table.
+SELECT last_name 
+  FROM actor;
+-- 1c. Select [all] columns from the film table.
+SELECT *
+  FROM film;
+  
+-- 2. DISTINCT operator
+
+-- 2a. Select all distinct last names from the actor table
+SELECT DISTINCT last_name
+  FROM actor;
+
+-- 2b. Select all distinct postal codes from the address table. 
+SELECT DISTINCT postal_code
+  FROM address;
+
+-- 2c. Select all distinct ratings from the film table. 
+SELECT DISTINCT rating
+  FROM film;
+
+-- 3. WHERE clause
+
+-- 3a. Select the title, description, rating, movie length 
+--     columns from the films table that last 3 hours or 
+--     longer.
+
+SELECT title, 
+       description, 
+       rating, 
+       length AS 'length (minutes)'
+  FROM film
+  WHERE length > 180;
+
+-- 3b. Select the payment id, amount, and payment date 
+--     columns from the payments table for payments made 
+--     on or after 05/27/2005.
+
+SELECT payment_id, amount, payment_date
+  FROM payment 
+  WHERE payment_date >=  '2005-05-27';
+
+
+-- 3c. Select the primary key, amount, and payment date 
+--     columns from the payment table for payments made 
+--     on 05/27/2005.
+
+SELECT payment_id, amount, payment_date
+  FROM payment 
+  WHERE payment_date LIKE '2005-05-27%';
+
+-- 3d. Select all columns from the customer table for rows 
+--     that have a last names beginning with S and a first 
+--     names ending with N.
+
+SELECT * 
+  FROM customer
+  WHERE last_name LIKE 'S%'
+    AND first_name LIKE '%N';
+
+-- 3e. Select all columns from the customer table for rows 
+--     where the customer is inactive or has a last name 
+--     beginning with "M".
+
+SELECT * 
+  FROM customer
+  WHERE last_name LIKE 'M%'
+     OR active = 0;
+
+-- 3f. Select all columns from the category table for rows 
+--     where the primary key is greater than 4 and the name
+--     field begins with either C, S or T.
+
+SELECT *
+  FROM category 
+  WHERE category_id > 4
+    AND SUBSTR(name, 1, 1) IN ('C', 'S', 'T');
+
+-- 3g. Select all columns minus the password column from the 
+--     staff table for rows that contain a password.
+
+SELECT staff_id, first_name, last_name, address_id, picture, 
+       email, store_id, active, username, last_update
+  FROM staff
+  WHERE password IS NOT NULL;
+
+-- 3h. Select all columns minus the password column from the 
+--     staff table for rows that do not contain a password.
+
+SELECT staff_id, first_name, last_name, address_id, picture, 
+       email, store_id, active, username, last_update
+  FROM staff
+  WHERE password IS NULL;
+
+-- 4. The IN operator
+
+-- 4a. Select the phone and district columns from the 
+--     address table for addresses in California, England, 
+--     Taipei, or West Java.
+
+SELECT phone, district
+  FROM address  
+  WHERE district IN ('California', 'England', 'Taipei', 'West Java');
+
+-- 4b. Select the payment id, amount, and payment date 
+--     columns from the payment table for payments made 
+--     on 05/25/2005, 05/27/2005, and 05/29/2005. 
+--     (Use the IN operator and the DATE function, instead 
+--     of the AND operator as in previous exercises.)
+
+SELECT payment_id, amount, payment_date
+  FROM payment 
+  WHERE DATE(payment_date) IN ('2005-05-25', '2005-05-27', '2005-05-29');
+
+-- 4c. Select all columns from the film table for films 
+--     rated G, PG-13 or NC-17.
+
+SELECT *
+  FROM film
+  WHERE rating IN ('G', 'PG-13', 'NC-17');
+
+-- 5. BETWEEN operator
+
+-- 5a. Select all columns from the payment table for 
+--     payments made between midnight 05/25/2005 and 
+--     1 second before midnight 05/26/2005.
+
+SELECT *
+  FROM payment 
+  WHERE payment_date BETWEEN '2005-05-25 00:00:00' 
+                         AND '2005-05-25 23:59:59';
+
+-- 5b. Select [all] columns from the film table for films 
+--     where the length of the description is between 100 
+--     and 120 
+
+SELECT *
+  FROM film
+  WHERE CHAR_LENGTH(description) BETWEEN 100 AND 120;
