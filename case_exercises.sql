7bdcb1924ab7b368f772e5197d11fad6e10a9609
Ben Smith <ben.f.smith07@gmail.com>
2022-01-13 15:23:58 -0600

    case_exercises.sql

--- case_exercises.sql
+++ case_exercises.sql
@@ -4,20 +4,18 @@
 --    that is a 1 if the employee is still with the 
 --    company and 0 if not. 
 
-SELECT e.emp_no AS 'Employee',
-       de.dept_no AS 'Department',
-       e.hire_date AS 'Start Date',
-       de.to_date AS 'End Date',
+SELECT e.emp_no AS employee,
+       de.dept_no AS department,
+       e.hire_date AS start_date,
+       de.to_date AS end_date,
        IF(de.to_date = '9999-01-01', True, False) as is_current_employee
-  FROM employees AS e
-    JOIN dept_emp AS de
-      ON e.emp_no = de.emp_no
+  FROM employees e
+    JOIN dept_emp de ON e.emp_no = de.emp_no
   WHERE (e.emp_no, de.to_date) 
         IN (
             SELECT e.emp_no, MAX(de.to_date)
-                FROM employees AS e
-                JOIN dept_emp AS de
-                    ON e.emp_no = de.emp_no
+                FROM employees e
+                JOIN dept_emp de ON e.emp_no = de.emp_no
                 GROUP BY e.emp_no 
            );
 
@@ -33,6 +31,19 @@ SELECT CONCAT(first_name, ' ', last_name) AS 'Employee',
          WHEN SUBSTR(last_name, 1, 1) IN ('I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q')
            THEN 'I-Q'
          WHEN SUBSTR(last_name, 1, 1) IN ('R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
+           THEN 'R-Z'
+         END AS 'alpha_group'
+  FROM employees;
+
+-- MORE ELEGANT SOLUTION:
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'Employee',
+       CASE
+         WHEN LEFT(last_name, 1) <= 'H'
+           THEN 'A-H'
+         WHEN LEFT(last_name, 1) <= 'Q'
+           THEN 'I-Q'
+         WHEN LEFT(last_name, 1) <= 'Z'
            THEN 'R-Z'
          END AS 'alpha_group'
   FROM employees;
@@ -54,7 +65,14 @@ SELECT COUNT(*),
   FROM employees
   GROUP BY decade;
 
--- BONUS: What is the current average salary for each of 
+-- MORE ROBUST SOLUTION (works for any decade):
+
+SELECT CONCAT(LEFT(birth_date, 3), '0s') AS decade,
+       COUNT(*)
+  FROM employees
+  GROUP BY decade;
+
+-- 4. What is the current average salary for each of 
 --        the following department groups: R&D, Sales & 
 --        Marketing, Prod & QM, Finance & HR, Customer 
 --        Service? 
@@ -64,14 +82,14 @@ SELECT
          WHEN dept_name IN ('Research', 'Development') THEN 'R&D'
          WHEN dept_name IN ('Sales', 'Marketing') THEN 'Sales & Marketing' 
          WHEN dept_name IN ('Production', 'Quality Management') THEN 'Prod & QM'
+         WHEN dept_name IN ('Human Resources', 'Finance') THEN 'Finance & HR'
          ELSE dept_name
          END AS dept_group,
-       AVG(s.salary) AS 'Average Salary',
-  FROM salaries AS s 
-    JOIN employees AS e 
-      ON s.emp_no = e.emp_no
-    JOIN dept_emp AS de 
-      ON e.emp_no = de.emp_no 
-    JOIN departments AS d 
-      ON de.dept_no = d.dept_no
+       AVG(s.salary) AS avg_salary
+  FROM salaries s 
+    JOIN employees e ON s.emp_no = e.emp_no
+    JOIN dept_emp de ON e.emp_no = de.emp_no 
+    JOIN departments d ON de.dept_no = d.dept_no
+  WHERE s.to_date = '9999-01-01'
+    AND de.to_date = '9999-01-01'
   GROUP BY dept_group;
